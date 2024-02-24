99b3fec74e8a3755aa606b714c5a5e6ba31b6b42
Exostars <julien.colleter@outlook.com>
2024-02-24 13:22:41 +0100

    MongoDB configuration

--- rankanim-api/app.js
+++ rankanim-api/app.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const appVersion = '1.0.0'
 
+// MongoDB connexion
+mongoose
+    .connect(
+        'mongodb+srv://exostars:ayase@exostarscluster.ysrkpqs.mongodb.net/?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+    )
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.get('/api/version', (req, res) => {
    res.json({ version: appVersion }); 
+});
+
+// Access config
+app.use((req, res, next) => {
+    const allowedOrigins = ['http://127.0.0.1:4200', 'http://127.0.0.1:3000'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    );
+    next();
 });
 
 module.exports = app;
